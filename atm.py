d105e3f70d63ec368fd0026890dc4c45c116a0d6
venkateshsiri45-wq <venkateshsiri45@gmail.com>
2025-09-25 10:37:51 +0530

    25/09/2025

---
+++ atm.py
@@ -0,0 +1,9 @@
+s=int(input("Enter your P assword="))
+while True:
+    if (s==2004):
+        print("correct Password")
+        print("Withdrawl or Deposit")
+        break
+    else:
+        print("Invalid Password")
+        break
