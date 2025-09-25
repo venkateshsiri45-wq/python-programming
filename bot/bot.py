9f3cc4045db6048b6f0376ee27420c8b83888b31
venkateshsiri45-wq <venkateshsiri45@gmail.com>
2025-09-25 11:28:28 +0530

    25/09/2025

---
+++ bot/bot.py
@@ -0,0 +1,11 @@
+while True:
+    user = input("you: ")
+    if user.lower() == "hi":
+        print("bot:hello how are you")
+    elif user.lower() == "fine":
+        print("bot:what next")
+    elif user.lower() == "iam happy":
+        print("bot:oh! wow what's the reason")
+    else:
+        print("bot:good bye")
+        
