7a8628bb13bd3b8ffb996fb7b23083b081676b9f
venkateshsiri45-wq <venkateshsiri45@gmail.com>
2025-09-26 10:41:09 +0530

    26/09/2025

--- marks/marks.py
+++ marks/marks.py
@@ -1,9 +1,9 @@
 n=int(input("how many students?"))
 student={}
 for i in range(n):
-      name=input(f"enter name of the student{i+1}:")
-      mark=int(input(f"enter mark of{name}:"))
-      student[name]=mark
-      print("\n dictionary:",student)
-      topper=max(student,key=student.get)
-      print("topper:",topper,"with",student[topper],"marks")
+    name=input(f"enter name of the student{i+1}:")
+    mark=int(input(f"enter mark of{name}:"))
+    student[name]=mark
+    print("\n dictionary:",student)
+    topper=max(student,key=student.get)
+    print("topper:",topper,"with",student[topper],"marks")
